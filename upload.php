a473d464390c4c6d0bf2fd44707110116a9536d8
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2020-02-18 01:16:27 +0200

    moving files to xml_files dir

--- upload.php
+++ upload.php
@@ -10,21 +10,32 @@ if(isset($_POST["upload"])) //submit button name
     {
         $file_name = $_FILES['zip_file']['name'];  
         
-           $array = explode(".", $file_name); 
-           //to separate extension "." from the other dots
-           $sliced_array = array_slice($array, 0, -1); 
-           $name = implode($sliced_array); 
-           $ext = $array[sizeof($array)-1];  //index of last element
-           if($ext == 'zip')  
-           { 
-            $path = 'upload/';  
-            $location = $path . $file_name; 
-            $tmp_name = $_FILES['zip_file']['tmp_name'];
-            if(move_uploaded_file($tmp_name, $location))
-              {
-                echo "moved";
-                } 
-            }
+        $array = explode(".", $file_name); 
+        //to separate extension "." from the other dots
+        $sliced_array = array_slice($array, 0, -1); 
+        $name = implode(".",$sliced_array); 
+        $ext = $array[sizeof($array)-1];  //index of last element
+        if($ext == 'zip')  
+        { 
+         $path = 'upload/';  
+         $location = $path . $file_name; 
+         $tmp_name = $_FILES['zip_file']['tmp_name'];
+         if(move_uploaded_file($tmp_name, $location))
+          {
+            $zip = new ZipArchive;  
+            if($zip->open($location))  
+            {  
+            $zip->extractTo($path);  
+            $zip->close();  
+            }  
+
+            $xml_file_name = $name.".xml";
+            copy($path.$xml_file_name,'xml_files/'.$xml_file_name);
+            unlink($path.$xml_file_name);
+            unlink($location);
+        
+          } 
+        }
         
     }
     else
