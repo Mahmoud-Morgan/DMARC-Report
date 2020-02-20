8d1324ee31df6b6d23c7f2dc8fb0ca2e6f69ff31
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2020-02-20 14:23:13 +0200

    edit heroku DB

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@ function OpenCon()
     $dbhost = "eu-cdbr-west-02.cleardb.net";
     $dbuser = "bf74ebbc73e60f";
     $dbpass = "5b68bb6f";
-    $db = " heroku_b3dd5bcc44be8e6";
+    $db = "heroku_b3dd5bcc44be8e6";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
     return $conn;
