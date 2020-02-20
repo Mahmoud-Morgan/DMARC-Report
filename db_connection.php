8534571efe996eb8b42fa53a076e50f02d6da22e
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2020-02-20 14:16:59 +0200

    add heroku DB

--- db_connection.php
+++ db_connection.php
@@ -1,15 +1,14 @@
 <?php
 function OpenCon()
 {
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $db = "DMARC-task";
+    $dbhost = "eu-cdbr-west-02.cleardb.net";
+    $dbuser = "bf74ebbc73e60f";
+    $dbpass = "5b68bb6f";
+    $db = " heroku_b3dd5bcc44be8e6";
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
     return $conn;
 }
-
 function CloseCon($conn)
 {
     $conn->close();
